b2afd0fa4de77f703a283f73c64d11d95ac7781b
ahyun04 <ahyun125@g.shingu.ac.kr>
2025-03-30 00:49:18 +0900

    [Feat] Excel파일 json화, Scene 로드

---
+++ Assets/Scripts/ItemType.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemType
+{
+    coffee,
+    smoothie,
+    juice,
+    machine,
+    weapon,
+    charater,
+    tool,
+}
