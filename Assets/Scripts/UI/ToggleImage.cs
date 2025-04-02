11911fb93e006a885a74f1e6e6874be1c33093eb
ahyun04 <ahyun125@g.shingu.ac.kr>
2025-04-03 00:03:46 +0900

    [Feat] recipe button 추가, recipe menu 숨김 처리

---
+++ Assets/Scripts/UI/ToggleImage.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleImage : MonoBehaviour
+{
+    public GameObject hiddenImage; // Inspector에서 연결할 이미지
+
+    void Start()
+    {
+        if (hiddenImage != null)
+            hiddenImage.SetActive(false); // 시작할 때 숨김
+    }
+
+    public void ToggleImageVisibility()
+    {
+        if (hiddenImage != null)
+            hiddenImage.SetActive(!hiddenImage.activeSelf); // 상태 반전
+    }
+}
