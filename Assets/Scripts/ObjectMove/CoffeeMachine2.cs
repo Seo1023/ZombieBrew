0fbbfe09e03364dcb65d0b580e18a85a93fcce9d
Im-Dinosaur <wnsdl80100@gmail.com>
2025-04-14 17:57:46 +0900

    [Feat] 커피머신 CS 초안 제작 및 커피머신 UI 제작

---
+++ Assets/Scripts/ObjectMove/CoffeeMachine2.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoffeeMachine2 : MonoBehaviour
+{
+    public bool isBrewing = false; //커피머신 작동 상태
+    public GameObject inventory; //인벤토리 UI
+    public GameObject combineUI; //조합 UI
+    public Transform player;  //플레이어 위치
+
+    //시작 시 UI 비활성화
+    void Start()
+    {
+        inventory.SetActive(false);
+        combineUI.SetActive(false);
+    }
+
+    public void Inventory()
+    {
+        
+    }
+
+    //추출 버튼 눌렀을 때 추출 되면서 인벤토리 안에 있는 아이템 제거 및 커피 제작
+    public void Brewing()
+    {
+        
+    }
+
+    // F로 상호작용 시 UI 키고 끔
+    void Update()
+    {
+        float distance = Vector3.Distance(transform.position , player.position);
+        if(distance < 2 && Input.GetKeyDown(KeyCode.F))
+        {
+            inventory.SetActive(!inventory.activeSelf);
+            combineUI.SetActive(!combineUI.activeSelf);
+        }
+    }
+
+
+}
