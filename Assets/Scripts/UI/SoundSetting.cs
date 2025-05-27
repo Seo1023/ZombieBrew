2beb945f1dd4e33d5f9bd5288b35769964cc130c
ahyun04 <ahyun125@g.shingu.ac.kr>
2025-05-27 23:51:22 +0900

    [Fix] 사운드 세팅 수정

--- Assets/Scripts/UI/SoundSetting.cs
+++ Assets/Scripts/UI/SoundSetting.cs
@@ -10,13 +10,35 @@ public class SoundSetting : MonoBehaviour
     public Slider bgmSlider;
     public Slider sfxSlider;
 
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        // 슬라이더 값 불러오기 (없으면 기본값 1.0f)
+        bgmSlider.value = PlayerPrefs.GetFloat("BGMVolume", 1.0f);
+        sfxSlider.value = PlayerPrefs.GetFloat("SFXVolume", 1.0f);
+
+        // 오디오 매니저에도 적용
+        AudioManager.instance.MusicVolume(bgmSlider.value);
+        AudioManager.instance.SFXVolume(sfxSlider.value);
+    }
+
     public void MusicVolume()
     {
         AudioManager.instance.MusicVolume(bgmSlider.value);
+
+        // 값 저장
+        PlayerPrefs.SetFloat("BGMVolume", bgmSlider.value);
     }
 
     public void SFXVolume()
     {
         AudioManager.instance.SFXVolume(sfxSlider.value);
+
+        // 값 저장
+        PlayerPrefs.SetFloat("SFXVolume", sfxSlider.value);
     }
 }
